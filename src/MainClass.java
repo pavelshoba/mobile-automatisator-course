7220023f6c52ea60fcc42594b6ff97b80bf4202d
pavelshoba <pavel.shoba@monterosa.co.uk>
2021-12-05 13:07:01 +0300

    homework1

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class MainClass {
+import org.junit.Test;
+
+public class MainClass
+{
+    public int getLocalNumber()
+    {
+        return 14;
+    }
 }
+
+
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class MainClassTest {
+import org.junit.Test;
+
+public class MainClassTest extends MainClass {
+    @Test
+    public void testGetLocalNumber() {
+        int a = this.getLocalNumber();
+
+        if (a == 14) {
+            System.out.println("Метод возвращает нужно число");
+        } else {
+            System.out.println("Метод не вовращает нужное число");
+        }
+    }
 }
